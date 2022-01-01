41cbcf71e8321db5c497da0d3ce0b28156b7de8e
Liang Yesheng <liang2kl@outlook.com>
2022-01-01 12:43:53 +0800

    Add timestamp for posts with tags

--- mkdocs_blogging_plugin/plugin.py
+++ mkdocs_blogging_plugin/plugin.py
@@ -166,6 +166,7 @@ class BloggingPlugin(BasePlugin):
     def on_page_markdown(self, markdown, page, config, files):
         if "tags" in self.features and "tags" in page.meta:
             tags = page.meta["tags"]
+            page = self.with_timestamp(page)
             if isinstance(tags, list):
                 for tag in tags:
                     if tag not in self.tags:
@@ -207,22 +208,7 @@ class BloggingPlugin(BasePlugin):
         for dir in self.docs_dirs:
             dir_path = Path(dir)
             if dir_path in file_path.parents:
-                timestamp = None
-                if self.meta_time_format:
-                    if "time" in page.meta:
-                        timestamp = datetime.strptime(
-                            page.meta["time"], self.meta_time_format).timestamp()
-                    elif "date" in page.meta:
-                        timestamp = datetime.strptime(
-                            page.meta["date"], self.meta_time_format).timestamp()
-                if not timestamp:
-                    timestamp = self.util.get_git_commit_timestamp(
-                        page.file.abs_src_path, is_first_commit=self.sort["by"] != "revision")
-                page.meta["git-timestamp"] = timestamp
-                page.meta["localized-time"] = self.util.get_localized_date(
-                    timestamp, False, format=self.time_format, _locale=self.locale)
-                self.blog_pages.append(page)
-
+                self.blog_pages.append(self.with_timestamp(page))
                 break
 
     def on_post_page(self, output, page, config):
@@ -275,7 +261,24 @@ class BloggingPlugin(BasePlugin):
         )
 
     def sorted_pages(self, pages):
-        # print(pages[0].__dict__)
         return sorted(pages,
                       key=lambda page: page.meta["git-timestamp"],
                       reverse=self.sort["from"] == "new")
+
+    def with_timestamp(self, page):
+        timestamp = None
+        if self.meta_time_format:
+            if "time" in page.meta:
+                timestamp = datetime.strptime(
+                    page.meta["time"], self.meta_time_format).timestamp()
+            elif "date" in page.meta:
+                timestamp = datetime.strptime(
+                    page.meta["date"], self.meta_time_format).timestamp()
+        if not timestamp:
+            timestamp = self.util.get_git_commit_timestamp(
+                page.file.abs_src_path, is_first_commit=self.sort["by"] != "revision")
+        page.meta["git-timestamp"] = timestamp
+        page.meta["localized-time"] = self.util.get_localized_date(
+            timestamp, False, format=self.time_format, _locale=self.locale)
+
+        return page
